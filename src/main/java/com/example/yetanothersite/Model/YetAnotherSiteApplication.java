8f78566c6e71bfdfd729074027731fd3ba88cd18
GpIIV <akselvolkov40@gmail.com>
2025-01-08 15:42:12 +0500

    q

---
+++ src/main/java/com/example/yetanothersite/Model/YetAnotherSiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.yetanothersite.Model;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YetAnotherSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YetAnotherSiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/yetanothersite/YetAnotherSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yetanothersite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YetAnotherSiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
